2d35749738eaf092f33587976eb7c00cb8c5f2f7
Marie-Pierre Lessard <male9@elev.techcollege.dk>
2026-01-06 14:44:40 +0100

    Delivery of CSS modules for 3 components

--- ROOT/src/components/g-footer/g-footer.tsx
+++ ROOT/src/components/g-footer/g-footer.tsx
@@ -1,7 +1,10 @@
+import footerstyle from "./g-footer.module.scss";
+
 export const GlobalFooterByMariePierreLessard = () => {
     return (
-        <footer>
-            <p className="read-the-docs">Visit <a href="https://react.dev" target="_blank" rel="noopener noreferrer" spellCheck="false">React.dev</a> to learn more.</p>
+        <footer className={footerstyle.globalFooterGridByMariePierreLessard}>
+            <p className={footerstyle.readTheDocs}>Visit <a href="https://react.dev" target="_blank" rel="noopener noreferrer" spellCheck="false">React.dev</a> to learn more.</p>
+            <img src="./src/assets/react.svg" alt="React-logo" className={footerstyle.globalFooterLogoByMariePierreLessard}></img>
         </footer>
     );
 };
--- ROOT/src/components/g-header/g-header.tsx
+++ ROOT/src/components/g-header/g-header.tsx
@@ -1,14 +1,17 @@
+import typography from "../../styles/typography.module.scss";
+import headerstyle from "./g-header.module.scss";
+
 export const GlobalHeaderByMariePierreLessard = () => {
     return (
-        <header>
-            <h1>Fordele og ulemper med <span className="react-blue-by-marie-pierre-lessard" spellCheck="false">React</span>-biblioteket</h1>
+        <header className={headerstyle.headerGridByMariePierreLessard}>
+            <h1>Fordele og ulemper med <span className={typography.reactBlueByMariePierreLessard} spellCheck="false">React</span>-biblioteket</h1>
             {/* "andreypopp
             on Jun 27, 2014
             Contributor
             You should use dateTime, DOM properties are camelCased in React." 
             https://github.com/facebook/react/issues/1749
             */}
-            <p>Marie-Pierre Lessard, den <time dateTime="2026-01-05">5. januar 2026.</time></p>
+            <p className={headerstyle.bylineByMariePierreLessard}>Marie-Pierre Lessard, den <time dateTime="2026-01-05">5. januar 2026.</time></p>
         </header>
     );
 };
